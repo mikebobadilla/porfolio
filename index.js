af3d922d3ab903fdd7d203b3fc7d9b2d692b08bd
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-06-22 16:38:05 -0700

    added email support

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const Mailgun = require('mailgun').Mailgun;
+const mg = new Mailgun('key-12a25cdbbfe8b402aca6b53fb89f3f80');
+
+app.use('/', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/sendmail', (req, res) => {
+    mg.sendText('contactform@mikebobadilla.com',
+        'mike.c.bobadilla@gmail.com',
+        "Contact Form Submission From " + req.body.name,
+        "Name: " + req.body.name + "\n\n" + "Email: " + req.body.email + "\n\n Phone: " + req.body.phone + "\n\n" + req.body.message,
+        function(err){
+          if(err){
+            console.err(err);
+            return err;
+          } else {
+              res.send("Thanks for contacting me! I'll be in touch.!")
+          }
+        }
+    );
+});
+
+const server = app.listen(9000, ()=> {
+    let port = server.address().port;
+    console.log(`Server is running at ${port}`);
+});
